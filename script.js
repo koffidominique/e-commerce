769a048e28677e315354b6bf9f84697758f28e62
MarvynKoffi <marvynkoffi@192.168.1.8>
2024-12-23 02:42:51 +0000

    Fin de la partie Footer . Je suis dans la section Shopping cart essayant de masquer mes items dans la barre de recherche ayant pour icon fa-brands fa-shopping

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+let cartIcon = document.querySelectorAll('.cart_detail .fa-cart-shopping')
+let cartMenu = document.querySelectorAll('.car_container')
+let closeCart = document.querySelectorAll('.close_cart')
