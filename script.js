20ed326f1ab2ab4e789f8415a5350bfe5f150385
MarvynKoffi <marvynkoffi@192.168.1.8>
2024-12-23 06:15:05 +0000

    Problem avec le Javascript

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 
 
-let cartIcon = document.querySelectorAll('.cart_detail .fa-cart-shopping')
+let cartIcon = document.querySelectorAll('.cart_menu .fa-cart-shopping')
 let cartMenu = document.querySelectorAll('.car_container')
 let closeCart = document.querySelectorAll('.close_cart')
+
+cartIcon.forEach(cart =>{
+    cart.addEventListener('click',()=>{
+        cartMenu.forEach(cartItem =>{
+            cartItem.classList.toggle('show_cart_menu');
+        });
+    });
+});
+closeCart.forEach(cartClose =>{
+    cartClose.addEventListener('click',()=>{
+        cartMenu.forEach(cartItem =>{
+            cartItem.classList.toggle('show_cart_menu');
+        });
+    });
+});
